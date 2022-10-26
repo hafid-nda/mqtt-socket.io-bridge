3f037b80a7c587c0a3d69ad53e1a3e45175080a5
hafid-nda <zaeinyh@gmail.com>
2022-10-26 16:31:43 +0700

    commit

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const io = new Server(server);
 const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://157.230.255.247:1883')
 
+//prototype chat app
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html'  );
 });
 
+//listen connection from socketio client
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('chat message', (msg) => {
